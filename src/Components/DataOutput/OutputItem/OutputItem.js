486b426e03a556c786eb96efe745d4ea6270f41d
Tobiiadee <oluwatobi121@gmail.com>
2023-11-23 12:58:49 +0100

    Web deploy

--- src/Components/DataOutput/OutputItem/OutputItem.js
+++ src/Components/DataOutput/OutputItem/OutputItem.js
@@ -1,19 +1,23 @@
+/** @format */
+
 import React from "react";
-import styles from "./OutputItem.module.css"
+import styles from "./OutputItem.module.css";
 
 const OutputItem = (props) => {
-    // console.log(props.dataValue);
-    const name = props.dataValue.name;    
-    const age = props.dataValue.age + "YRS";   
+  // console.log(props.dataValue);
+  const name = props.dataValue.name;
+  const age = props.dataValue.age + "YRS";
 
-    // console.log(name, age);
+  // console.log(name, age);
 
-    return <div>
-        <div className={styles.OutputItem}>
-            <div className={styles["age-input"]}>{age}</div>
-            <input type="text" readOnly value={name}/>
-        </div>
+  return (
+    <div>
+      <div className={styles.OutputItem}>
+        <div className={styles["age-input"]}>{age}</div>
+        <input type='text' readOnly value={name} />
+      </div>
     </div>
+  );
 };
 
 export default OutputItem;
--- src/Components/UI/Card/Card.js
+++ src/Components/UI/Card/Card.js
@@ -4,7 +4,7 @@ import React from "react";
 import style from "./Card.module.css";
 
 const Card = (props) => {
-  const classes = props.className + style.Card;
+  const classes = `${props.className} ${style.Card}`;
 
   return <div className={classes}>{props.Children}</div>;
 };
